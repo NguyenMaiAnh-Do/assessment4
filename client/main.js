7d805b7923591419c3b7eff22cfa45181eeb71f7
Anh Do <anhdodr@gmail.com>
2024-01-27 11:04:31 -0800

    adding Feature 1 Fortune

--- client/main.js
+++ client/main.js
@@ -1,4 +1,5 @@
 const complimentBtn = document.querySelector("#complimentButton")
+const fortuneBtn = document.querySelector('#getFortune')
 
 const getCompliment = () => {
     axios.get("http://localhost:4000/api/compliment/")
@@ -8,4 +9,12 @@ const getCompliment = () => {
     });
 };
 
+const getFortune = () => {
+    axios.get("http://localhost:4000/api/compliment/")
+    .then(res => {
+        const data = res.data;
+        alert(data);
+    })
+}
 complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
